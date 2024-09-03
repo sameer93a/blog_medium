9b65ded4473593f5ad091d2d7b011e1f2ebede9b
Sameer Ahmed <sameer937655@gmail.com>
2024-09-03 19:45:11 +0530

    create middleware for extracting token from header + create blogh route

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,3 +1,68 @@
+import { createBlogInput } from "@sameer93a/medium_common";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const blogRouter = new Hono();
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  try {
+    const user = await verify(authHeader, c.env.JWT_SECRET);
+    if (user) {
+      c.set("userId", user.id as string);
+      await next();
+    } else {
+      return c.json(
+        {
+          msg: "You are not logged in",
+        },
+        403
+      );
+    }
+  } catch (e) {
+    return c.json(
+      {
+        msg: "You are not logged in",
+      },
+      403
+    );
+  }
+});
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = createBlogInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Inputs not correct",
+    });
+  }
+
+  const authorId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.blog.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: Number(authorId),
+    },
+  });
+
+  return c.json({
+    id: blog.id,
+  });
+});
